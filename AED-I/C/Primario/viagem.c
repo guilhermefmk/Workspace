9b2d682fcd6dd94169429d3a35f848fdfcc96482
Guile <guilhermefmk9@gmail.com>
2022-02-17 09:48:18 -0300

    A viagem

--- AED-I/C/Primario/viagem.c
+++ AED-I/C/Primario/viagem.c
@@ -6,20 +6,43 @@
 #include <math.h>
 
 int main(){
-    int n = 1, i; 
-    float valores[20], media, soma = 0;
 
+    int n = 1, i, cont = 0; 
+    float valores[500], media, soma = 0, somadif=0, resultados[500];
+    
+          
     while(n>0){
         scanf("%d", &n);
-        
-        for(i=0;i <n;i++){
-            float aux;
-            scanf(" %f", &aux);
-            valores[ i ] = aux;
-            soma += valores[ i ];
+        if(n > 0){
+
+
+            float somadif=0, soma = 0;
+
+
+            for(i = 0;i <n; i++){
+                float aux;
+                scanf(" %f", &aux);
+                valores[ i ] = aux;
+                soma += valores[ i ];
+            }
+
+
+            media = soma/n;
+
+
+            for(i = 0;i < n; i++){
+                if(valores[i]>media){
+                    somadif = somadif + (valores[i] - media);
+                }
+            }
+            resultados[cont] = somadif;
+            cont ++;
+
         }
-        media = soma/n;
-        printf("%f", media);
+    }
+
+    for(i = 0; i < cont; i++){
+        printf("%.2f\n", resultados[i]);
     }
     return 0;
 }
