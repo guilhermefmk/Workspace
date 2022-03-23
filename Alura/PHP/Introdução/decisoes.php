0c716c863da055ffce5632a29e2466ead9d9368d
Guile <guilhermefmk9@gmail.com>
2022-03-23 10:35:36 -0300

    ajuste

--- Alura/PHP/Introdução/decisoes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$idade = 15;
-$mensagem = ($idade < 18) ? "Você é menor de idade" : "Você é maior de idade";
-echo $mensagem;
-
-
--- Alura/PHP/Introdução/repeticoes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-for($i = 1; $i <= 15; $i++){
-    print("$i" . PHP_EOL);
-}
